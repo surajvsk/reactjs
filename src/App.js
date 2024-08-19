933a330bd3fb13609af4a781765c36a727d59ea8
Suraj Vishwakarma <suraj.vishwakarma.ext@nmims.edu>
2024-08-19 13:49:19 +0530

    export button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './componets/Button'; // Import Button component
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Button />
     </div>
   );
 }
---
+++ src/componets/Button.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class Button extends Component {
+    render() {
+      // reference as a js object
+      return <button >Error Button</button>;
+    }
+  }
+
+export default Button;
