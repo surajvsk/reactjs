74cb8b055599d1582a24d07455decec2c35d40ad
Suraj Vishwakarma <suraj.vishwakarma.ext@nmims.edu>
2024-08-19 14:10:02 +0530

    19

--- src/componets/Button.js
+++ src/componets/Button.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-class Button extends Component {
+import Button from '@mui/material/Button';
+
+class mButton extends Component {
     render() {
       // reference as a js object
-      return <button >Error Button</button>;
+      return <Button  variant="contained">Hello world!</Button>;
     }
-  }
+  };
 
-export default Button;
+
+export default mButton;
